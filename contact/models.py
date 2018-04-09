e607d788d94b639eb05bac29376c323ad7d50190
Matthew Sheinberg <msheinberg@gmail.com>
2018-04-09 16:20:25 -0400

    Corrected some errors in the contact database code and starting out on the contact view.

--- contact/models.py
+++ contact/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Contact(models.Model):
     first_name=models.CharField(max_length=200)
     last_name=models.CharField(max_length=200)
-    email=models.EmailField
+    email=models.EmailField(max_length=254)
     subject=models.CharField(max_length=200)
-    message=models.TextField
+    message=models.TextField()
 
     def __str__(self):
         return self.id
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request, ):
+    #makes a list of the last 5 contact requests
+    latest_contact_list = Contact.objects.order_by('-pubdate')[:5]
+    #will print out a list of the messages in the contact list
+    output=', 'join([q.message for q in latest_contact_list])
+    return HttpResponse(output) 
+
+def detail(request, contact_id):
+    return HttpResponse("You're looking at contact %s." % contact_id)
